15f7390c9b7ba0650084e7e44a6b8d3b26cb7f5c
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-08-06 15:57:19 -0400

    product item exported

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { pluralize } from '../../utils/helpers';
-import { useDispatch, useSelector } from 'react-redux';
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
-import { idbPromise } from '../../utils/helpers';
+import React from "react";
+import { Link } from "react-router-dom";
+import { pluralize } from "../../utils/helpers";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
+import { useSelector, useDispatch } from "react-redux";
 
 function ProductItem(item) {
+  const state = useSelector((state) => {
+    return state;
+  });
+
   const dispatch = useDispatch();
-  const state = useSelector((state) => state);
 
   const { image, name, _id, price, quantity } = item;
 
@@ -21,7 +24,7 @@ function ProductItem(item) {
         _id: _id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
-      idbPromise('cart', 'put', {
+      idbPromise("cart", "put", {
         ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
@@ -30,7 +33,7 @@ function ProductItem(item) {
         type: ADD_TO_CART,
         product: { ...item, purchaseQuantity: 1 },
       });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+      idbPromise("cart", "put", { ...item, purchaseQuantity: 1 });
     }
   };
 
@@ -42,7 +45,7 @@ function ProductItem(item) {
       </Link>
       <div>
         <div>
-          {quantity} {pluralize('item', quantity)} in stock
+          {quantity} {pluralize("item", quantity)} in stock
         </div>
         <span>${price}</span>
       </div>
