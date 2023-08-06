29280f33ab0db11e2e2b7b115fbf6f15546e916b
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-08-06 15:52:43 -0400

    if categoryData exists or has changed from the response of useQuery, then run dispatch()

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,32 +1,37 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_CATEGORIES } from "../../utils/queries";
+import { useSelector, useDispatch } from "react-redux";
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
-} from '../../utils/actions';
-import { QUERY_CATEGORIES } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+} from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 function CategoryMenu() {
+  const state = useSelector((state) => {
+    return state;
+  });
+
   const dispatch = useDispatch();
-  const state = useSelector((state) => state);
 
   const { categories } = state;
 
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
     if (categoryData) {
+      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
       dispatch({
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories,
       });
       categoryData.categories.forEach((category) => {
-        idbPromise('categories', 'put', category);
+        idbPromise("categories", "put", category);
       });
     } else if (!loading) {
-      idbPromise('categories', 'get').then((categories) => {
+      idbPromise("categories", "get").then((categories) => {
         dispatch({
           type: UPDATE_CATEGORIES,
           categories: categories,
